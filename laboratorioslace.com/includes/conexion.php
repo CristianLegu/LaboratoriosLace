dc04b30bbae8b6fed09d5a5a770ecace83987617
betoG7 <humbertogarciag7@gmail.com>
2017-01-06 12:46:33 -0600

    quitar head
    en conexion

--- laboratorioslace.com/includes/conexion.php
+++ laboratorioslace.com/includes/conexion.php
@@ -4,12 +4,6 @@ $host = "localhost";
 $user = "lace";
 $pwd = "root";
 $db = "lace";
-<<<<<<< HEAD
-//$host = "localhost";
-//$user = "lace";
-//$pwd = "root1";
-//$db = "bdlace";
-=======
->>>>>>> origin/master
+
 
 ?>
