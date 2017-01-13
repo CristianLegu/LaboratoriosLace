1707bb0b062bea36a7cd728fc2a6ee7408ce9a42
Mariano <ianmvc4@gmail.com>
2017-01-13 12:38:55 -0600

    Eliminar Subtitulo y valores del mismo

--- laboratorioslace.com/estudio.php
+++ laboratorioslace.com/estudio.php
@@ -16,8 +16,8 @@ foreach($_GET as $loc=>$item) $_GET[$loc] = urldecode(base64_decode($item));
   $cont = 1;
   $i = 1;
   $idmed = 0;?>
- 
-  
+
+
 				   <script language='javascript'>
 				  var i = 1;
           var vi = 1;
@@ -29,7 +29,7 @@ foreach($_GET as $loc=>$item) $_GET[$loc] = urldecode(base64_decode($item));
       <head>
            <title>Análisis</title>
            <script src="js/jquery.min.js"></script>
-            
+
  <link rel="stylesheet" type="text/css" media="all" href="css/styles.css">
   <?php if (isset($_GET['es'])){
 
@@ -95,7 +95,7 @@ if(isset($_GET['es'])){
 
       <div class="container">
         <div class="form-group">
-          <form name="add_name" id="add_name" method="post" action="guarda.php " ALIGN=center autocomplete="off">				
+          <form name="add_name" id="add_name" method="post" action="guarda.php " ALIGN=center autocomplete="off">
     	    <div class="col-2">
       			<label >
      					Nombre del Estudio
@@ -121,15 +121,15 @@ if(isset($_GET['es'])){
           </table>
           <hr>
           <div id="dynamic_field2">
-         
+
           </div>
 <?php
       }
       else{ $subt=""?>
          <table id="dynamic_field">
-           
+
   <?php  while (  $row = mysqli_fetch_array($fila, MYSQLI_ASSOC)) {
-  ?>   <?php  $renglon = "row".$i; 
+  ?>   <?php  $renglon = "row".$i;
   if($subt != $row ['subtitulo'] ){
     $subt =$row ['subtitulo'] ;
          ?>
@@ -143,19 +143,19 @@ if(isset($_GET['es'])){
               <td><input type="form-control" name="pruebas[]" placeholder="Prueba" class="form-control name_list" value="<?php  echo $row ['prueba'] ?> " /></td>
               <td><input type="form-control" name="unidades[]" placeholder="Unidades" class="form-control name_list" value="<?php  echo $row ['unidades'] ?> " /></td>
               <td><input  type="form-control" name="valorreferencia[]" placeholder="Valor de referencia" class="form-control name_list" value="<?php  echo $row ['valorreferencia'] ?> " /></td>
-             
+
                   <?php if ($cont == 1) { ?>
               <td><button type="button" name="add" id="add" class="agregar">Agregar</button></td>
                   <?php } else { ?>
               <td><button type="button" name="remove" id="<?php echo $i; ?>" class="eliminar btn_remove">X</button></td>
                     <?php } ?>
            </tr>
-           
+
   <?php $cont++; $i++;} ?>
       </table>
      	<?php	 }   $i ;   ?>
        </div>
-  
+
       <div class="col-submit button">
         <input name="idpropio" value="<?php if($fila1 != null) { echo $idpropio; } else {$idpropio = 0; echo $idpropio;}?>"  style='display:none;'>
         <input name="idpaciente" value = "<?php echo $idpac; ?>" style="display:none;">
@@ -166,7 +166,7 @@ if(isset($_GET['es'])){
     </div>
   </body>
  </html>
- 
+
  <script>
    var i = 0;
    var j= 0;
@@ -180,13 +180,13 @@ if(isset($_GET['es'])){
                '<tr id="row'+t+'">'+
                '<td><input type="form-control" name="pruebas[]" placeholder="Prueba'+t+'" class="form-control name_list" /></td>'+
                '<td><input type="form-control" name="unidades[]" placeholder="Unidades'+t+'" class="form-control name_list" /></td>'+
-               
+
                '<td><input type="form-control" name="valorreferencia[]" placeholder="Valor de referencia'+t+'" class="form-control name_list" /></td>'+
                '<td><input  type="form-control" name="idsubtitulo[]" value="'+vi+'" style="display:none"  class="form-control name_list" /></td>'+
                '<td><button type="button" name="remove" id="'+t+'" class="eliminar btn_remove">X</button></td></tr>');
       console.log("T"+t);
       });
-      
+
       $(document).on('click', '.btn_remove', function(){
            var button_id = $(this).attr("id");
            $('#row'+button_id+'').remove();
@@ -210,7 +210,7 @@ $('#addsub').click(function(){
             i++;
             j++;
             vi++;
-            
+
            $('#dynamic_field2').append(
                '<div id="sub'+j+'">'+
                '<div style="display: flex;margin-top: 10px;">'+
@@ -233,7 +233,7 @@ $('#addsub').click(function(){
       });
 
       $(document).on('click', '.agregar2', function(){
-    
+
           var button_id = $(this).attr("id");
          console.log(button_id);
           i = arreglo[button_id];
@@ -242,11 +242,11 @@ $('#addsub').click(function(){
                '<div id="reng'+button_id+'-'+i+'"style="display: flex;">'+
                     '<input type="form-control" name="pruebas[]" placeholder="Prueba'+i+'" class="form-control name_list" />'+
                     '<input type="form-control" name="idsubtitulo[]" value="'+vi+'" style="display:none;" class="form-control name_list" />'+
-                  
+
                     '<input type="form-control" name="unidades[]" placeholder="Unidades'+i+'" class="form-control name_list" />'+
                     '<input type="form-control" name="valorreferencia[]" placeholder="Valor de referencia'+i+'" class="form-control name_list" />'+
                     '<button type="button" name="remove" id="'+button_id+'-'+i+'" class="eliminar btn_remove2">X</button></div>');
-           
+
            arreglo.splice(button_id, 1, i);
            i=0;
            console.log("i"+i);
@@ -256,13 +256,7 @@ $('#addsub').click(function(){
 
       $(document).on('click', '.btn_remove2', function(){
            var button_id = $(this).attr("id");
-           var aux= button_id.substring(0, 1);//Arreglar
-           var aux2= button_id.substring(2, 3);//arreglar
-           console.log(aux+""+aux2);
-            i = arreglo[aux];
-            i--;
-           arreglo.splice(aux, 1, i);
-           $('#reng'+aux+'-'+aux2+'').remove();
+           $('#reng'+button_id+'').remove();
            vi--;
            console.log("I"+i);
            console.log("j"+j);
@@ -270,11 +264,14 @@ $('#addsub').click(function(){
       });
       $(document).on('click', '.btn_removesub', function(){//arreglar
            var button_id = $(this).attr("id");
+           var aux=button_id.split("-", 1);
            $('#sub'+button_id+'').remove();
-           j--;
+           //j--;
            vi--;
-           console.log("J"+j);
+           arreglo.splice(aux, 1, "");
+           console.log("J"+aux);
+           console.log("J"+arreglo);
       });
  });
- 
+
  </script>
